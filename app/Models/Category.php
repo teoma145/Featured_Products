776ae5b73c2ad9ec37ca6a1591367b09a18f9ec4
teoma145 <matteodark145@yahoo.it>
2024-03-12 11:18:54 +0100

    model-relation and foreign key

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -10,4 +10,8 @@ class Category extends Model
     use HasFactory;
 
     protected $fillable = ['name', 'color'];
+    public function products()
+    {
+        return $this->hasMany(Product::class);
+    }
 }
