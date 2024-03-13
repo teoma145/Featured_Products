ba1752fbdd9ecd6b2a93b49c4b58ea93f1351393
DaniloSalerno <salerno.danilo@hotmail.it>
2024-03-13 11:08:27 +0100

    add product card

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,5 +2,18 @@
 @section('content')
     <section class="container">
         <h1>Products Store</h1>
+        <div class="row row-cols-1 row-cols-md-2 row-cols-lg-4 row-cols-xl-5">
+            @forelse($highlighted_products as $product)
+                <div class="col">
+                    @include('partials.product-card')
+                </div>
+            @empty
+                <div class="col">
+                    <div class="card">
+                        No products available
+                    </div>
+                </div>
+            @endforelse
+        </div>
     </section>
 @endsection
