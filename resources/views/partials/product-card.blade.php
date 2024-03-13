b01e0886aa72530b1667c035ca51893ff8d73ebb
DaniloSalerno <salerno.danilo@hotmail.it>
2024-03-13 11:36:53 +0100

    complete product card

--- resources/views/partials/product-card.blade.php
+++ resources/views/partials/product-card.blade.php
@@ -1,21 +1,21 @@
-<div class="card border-0">
-    <h2 style="color: {{$categories[$product->category_id - 1]['color']}} ">
+<div class="card bg-transparent border-0 text-white">
+    <h4 style="color: {{$categories[$product->category_id - 1]['color']}} ">
         <div>{{$categories[$product->category_id - 1]['name']}}</div>
-        <div>
-            {{$categories[$product->category_id - 1]['color']}}
-        </div>
-    </h2>
-    <div class="price">
+    </h4>
+    <div class="product_price">
         {{$product->price}} €
     </div>
-    <div>
+
+    <div class="product_name">
         {{$product->name}}
     </div>
-    <div>
+
+    <div class="product_description">
         {{$product->description}} 
     </div>
+
     <div>
-        <button class="btn">
+        <button class="btn btn-outline-light">
             get started
         </button>
     </div>
