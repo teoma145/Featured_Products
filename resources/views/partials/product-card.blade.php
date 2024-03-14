7cefaea5bc6a6c9647286d55afe4e0ebe27ca10a
DaniloSalerno <salerno.danilo@hotmail.it>
2024-03-14 11:07:41 +0100

    change card product design

--- resources/views/partials/product-card.blade.php
+++ resources/views/partials/product-card.blade.php
@@ -2,8 +2,13 @@
     <h4 style="color: {{$categories[$product->category_id - 1]['color']}} ">
         <div>{{$categories[$product->category_id - 1]['name']}}</div>
     </h4>
-    <div class="product_price">
-        {{$product->price}} €
+    <div class="product_price d-flex justify-content-center align-items-center gap-1">
+        <div class="euro"> <span class="fs-2 pe-1">$</span>{{ explode(".", $product->price)[0]}}</div>
+        <div class="mb-2">
+            <div class="cent">{{ explode(".", $product->price)[1]}}</div>
+            <div class="period pt-2">/MO</div>
+            
+        </div>
     </div>
 
     <div class="product_name">
